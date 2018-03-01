a039934b968de44f33618f5094894e59abd9ad1b
Professor Andre Silva <andre@fiar.local>
2018-02-28 22:06:25 -0300

    pronto insert

--- src/br/com/proj/dados/MotoDB.java
+++ src/br/com/proj/dados/MotoDB.java
@@ -1,13 +1,21 @@
 package br.com.proj.dados;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import br.com.proj.entidade.Moto;
+import br.com.proj.util.Conexao;
 
 public class MotoDB implements IMotoDB{
 	
+	private Connection con;
+	private PreparedStatement ps;
+	
 	public MotoDB(){
 		//ABERTURA DA CONEXÃO COM A BASE DE DADOS
+		con = Conexao.getConnection();
 	}
 
 	@Override
@@ -19,7 +27,33 @@ public class MotoDB implements IMotoDB{
 		
 		//IMPLEMENTAÇÃO DO INSERT NA BASE DE DADOS
 		
-		return true;
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(" INSERT INTO MOTO ");
+        sb.append(" (id, descricao, ano) values (?, ?, ?) ");
+        
+        try {
+			ps = this.con.prepareStatement(sb.toString());
+		
+	        ps.setInt(1, moto.getId());
+	        ps.setString(2, moto.getDescricao());
+	        ps.setInt(3, moto.getAno());
+	        
+	        ps.execute();
+	        
+	        return true;
+        
+        } catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+        
+        return false;
 	}
 
 	@Override
